02dd11ecb16518de923dd50985be15b13c778516
sabz19 <nsabarish19@gmail.com>
2018-01-25 12:17:31 -0600

    Made changes to loginScraper.py

--- Web Scraping/loginScraper.py
+++ Web Scraping/loginScraper.py
@@ -1,10 +1,15 @@
-# Program to login to utdallas web page and retrieve all related HTML PAGE
+""" 
+	Program to login to utdallas web page and retrieve all related HTML PAGE
+"""
 
 import requests,lxml.html
 import os
 from lxml import etree
 from bs4 import BeautifulSoup
 
+"""
+	The login url is a redirect from the base url 
+"""
 BASE_URL = "https://provost.utdallas.edu/ues-report/table/term_17f,col_ecs,heat_none"
 
 LOGIN_PAGE = requests.get(BASE_URL)
@@ -16,6 +21,9 @@ login = session.get(LOGIN_URL)
 login_html = lxml.html.fromstring(login.text)
 hidden_inputs = login_html.xpath(r'//form//input[@type = "hidden"]')
 
+
+#Fetch all hidden form parameters
+
 form = {x.attrib["name"]:x.attrib["value"] for x in hidden_inputs}
 form['netid'] = '##########'
 form['password'] = '########'
@@ -24,6 +32,9 @@ response = session.post(LOGIN_URL,data = form)
 
 RESULT_PAGE = session.get(BASE_URL)
 
+
+# Fetch all table information and store it in a text file for later processing 
+# This essentially entails the course/professor data required
 
 soup = BeautifulSoup(RESULT_PAGE.text,'lxml')
 table = soup.find('table')
@@ -37,38 +48,3 @@ for row in table_rows:
 	for table_data in row:
 		f.write(str(table_data))
 	f.write("\n")
-
-
-
-
-
-
-
-
-# table = etree.HTML(RESULT_PAGE.text).find("body/table")
-# rows = iter(table)
-# headers = [col.text for col in next(rows)]
-
-# for row in rows:
-# 	values = [col.text for col in row]
-# 	print dict(zip(headers,values))
-
-
-
-
-
-# payload = {
-
-# 	'username' : '######','password' : '#####'
-# }
-
-# cj = cookielib.CookieJar()
-# opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-# login_data = urllib.urlencode({'netid':username,'password':password})
-# print(login_data)
-# opener.open('https://provost.utdallas.edu/ues-report/',login_data)
-# resp = opener.open('https://provost.utdallas.edu/ues-report/table/term_17f,heat_none')
-
-
-
-
